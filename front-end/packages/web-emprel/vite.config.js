9fa79a38bc116bf1b416d859e8f714b919af7124
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2025-08-09 16:49:57 -0300

    Mudança no vite config

--- front-end/packages/web-emprel/vite.config.js
+++ front-end/packages/web-emprel/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Esta configuração padrão está correta para o projeto principal.
 export default defineConfig({
   plugins: [react()],
+  base: '/emprel/',
 })
